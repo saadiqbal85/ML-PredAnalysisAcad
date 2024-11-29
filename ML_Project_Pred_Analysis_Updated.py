813bada6192970a0497fc6a68103415a1a0838dd
Saad Iqbal <48171415+saadiqbal85@users.noreply.github.com>
2024-11-29 22:14:29 +0500

    Update ML_Project_Pred_Analysis_Updated.py

--- ML_Project_Pred_Analysis_Updated.py
+++ ML_Project_Pred_Analysis_Updated.py
@@ -4,6 +4,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 
